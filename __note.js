433e75652615162c2be413659c784ee4068ceaa9
Faisal Ahmed <faisal2410@yahoo.com>
2022-09-16 19:22:50 +0600

    initial commit

---
+++ __note.js
@@ -0,0 +1,40 @@
+/**
+ * curl -v localhost:3000
+ * curl -v localhost:3000/node.png --output -
+ *  http message :
+        // 1. start-line - CHECK
+        // 2. header
+        // 3. body
+ * http is native to NodeJS. We just have to ask for it
+ *  the http module has a createServer method
+// takes 1 arg:
+// 1. callback, callback, has 2 args: req, res* 
+// createServer returns an object with a listen method
+// listen takes 1 arg:
+// 1. port to listen for http traffic on
+ * 
+ * */ 
+
+/**
+ * fs = file system module! fs is built into Node. see above
+// fs gives node access to THIS computers file system.
+ * 
+ * 
+ * 
+ * */ 
+
+/**
+ * the user wants the home page! we know, because the req object has / in the url property
+        // console.log(req)
+        // res = our way of responding to the requester
+        // http message 
+        // 1. start-line - CHECK
+        // 2. header
+        // 3. body
+        // writeHead takes 2 args:
+        // 1. status code
+        // 2. object for the mime-type
+ * 
+ * 
+ * 
+ * */ 
